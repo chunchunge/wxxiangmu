d1713f86771d8496e70ce8b2ae7e53c509ba174c
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-29 20:44:43 +0800

    格式化

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,10 +1,10 @@
-import request from "../../request/request"
+import request from "../../request/request";
 Page({
-  // wxml中 只能找到 data中的变量的数据 
-  // data 中应该只存放 视图渲染 要用到的数据  
+  // wxml中 只能找到 data中的变量的数据
+  // data 中应该只存放 视图渲染 要用到的数据
   // data中的数据越多 页面越卡！！！
   // 视图要使用的全局数据
-  data:{
+  data: {
     // 左侧的内容
     leftMenus: [],
     // 右侧的内容 列表
@@ -15,27 +15,25 @@ Page({
   },
   // 全局数据
   Cates: [],
-  onLoad(){
-    request({url:"categories"}).then(res=>{
+  onLoad() {
+    request({ url: "categories" }).then(res => {
       this.Cates = res.data.message;
       this.setData({
         // 获取对象中的某一项
-        leftMenus:this.Cates.map(v=>v.cat_name),
-        rightGoods:this.Cates[this.data.currentIndex].children
-        
-      })
+        leftMenus: this.Cates.map(v => v.cat_name),
+        rightGoods: this.Cates[this.data.currentIndex].children
+      });
       console.log(this.data.rightGoods);
-    })
+    });
   },
-    handleMenuTap(e) {
-      const currentIndex = e.currentTarget.dataset.index;
-      this.setData({
-        currentIndex,
-        // 右侧的内容
-        rightGoods: this.Cates[currentIndex].children,
-        // 控制右侧列表的滚动条的 距离的！！
-      scrollTop:0
-      })   
-      
+  handleMenuTap(e) {
+    const currentIndex = e.currentTarget.dataset.index;
+    this.setData({
+      currentIndex,
+      // 右侧的内容
+      rightGoods: this.Cates[currentIndex].children,
+      // 控制右侧列表的滚动条的 距离的！！
+      scrollTop: 0
+    });
   }
-})
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,3 @@
-
 /* 
 * 1 轮播图 
     0 在页面的onLoad事件中来发送异步请求 
@@ -12,42 +11,35 @@
 //  引入统一发送异步请求的函数
 import request from "../../request/request";
 Page({
-    data :{
-        // 轮播图数组
-        swiperList:[],
-        // 导航数组
-        navsList:[],
-        // 楼层数组
-        floorList:[]
-    },
-    onLoad(){
-        // 发送异步请求
-        request({ url: "home/swiperdata" })
-        .then(result =>{
-                // console.log(result.data.message);
-                this.setData({
-                    swiperList:result.data.message
-                })
-            
-        }),
-        // 导航请求
-        request({ url: "home/catitems" })
-      .then(result => {
-                // console.log(result.data.message);
-                 this.setData({
-                    navsList:result.data.message
-                })
-            
-        }),
-        // 请求楼层
-        request({ url: "home/floordata" })
-      .then(result => {
-                console.log(result.data.message);
-                this.setData({
-                    floorList:result.data.message
-                })
-            
+  data: {
+    // 轮播图数组
+    swiperList: [],
+    // 导航数组
+    navsList: [],
+    // 楼层数组
+    floorList: []
+  },
+  onLoad() {
+    // 发送异步请求
+    request({ url: "home/swiperdata" }).then(result => {
+      // console.log(result.data.message);
+      this.setData({
+        swiperList: result.data.message
+      });
+    }),
+      // 导航请求
+      request({ url: "home/catitems" }).then(result => {
+        // console.log(result.data.message);
+        this.setData({
+          navsList: result.data.message
         });
-          
-    }
-})
+      }),
+      // 请求楼层
+      request({ url: "home/floordata" }).then(result => {
+        console.log(result.data.message);
+        this.setData({
+          floorList: result.data.message
+        });
+      });
+  }
+});
