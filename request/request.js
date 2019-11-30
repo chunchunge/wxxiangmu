7d7f922b41af7d9ba694c3567b9e5beff18a5c24
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-30 19:24:22 +0800

    封装加载中效果

--- request/request.js
+++ request/request.js
@@ -1,11 +1,30 @@
+//  定义一个变量 记录同时发送请求的个数
+let ajaxCount = 0;
+
 function axios({ url, ...params }) {
+  ajaxCount++;
     const baseURL = "https://api.zbztb.cn/api/public/v1/";
     return new Promise((resolve, reject) => {
+       // 显示加载中 
+    wx.showLoading({
+      title: "加载中",
+      mask: true
+    });
       wx.request({
         url: baseURL + url,
         ...params,
         success: (result) => {
           resolve(result);
+        },
+        complete(){
+          ajaxCount--;
+          if (ajaxCount === 0) {
+            // 都回来了
+            // console.log(ajaxCount);
+            // console.log("最后一个请求回来了");
+            // 不管请求是否成功 都会触发的回调函数
+            wx.hideLoading();
+          }
         }
       });
     })
