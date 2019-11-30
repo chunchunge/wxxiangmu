f47c4336bb0dc8b8ebeb23b59da475c0fe9c85c0
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-30 19:41:22 +0800

    轮播图渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,24 @@
-// pages/goods_detail/index.js
+import request from "../../request/request";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsInfo:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getDetail(options.goods_id);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取商品的详情数据
+  getDetail(goods_id) {
+    request({
+      url: "goods/detail",
+      data: {
+        goods_id
+      }
+    }).then(res => {
+      console.log(res);
+      this.setData({
+        goodsInfo:res.data.message
+      })
+    })
   }
 })
