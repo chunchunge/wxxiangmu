d3464eb8a9bafbbba146f5ba5bf8af413366dd52
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-12-03 19:11:18 +0800

    渲染购物车页面

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -33,21 +33,19 @@ Page({
     // 用户收货地址
     address: {},
     // 购物车数组
-    carts:[]
-  },
-  onLoad(){
-    const carts=wx.getStorageSync("carts")||[];
-    this.setData({
-      carts
-    })
+    carts:[],
+    // 商品的总价格
+    totalPrice: 0
   },
   // 监听页面的显示
   onShow() {
+    const carts=wx.getStorageSync("carts")||[];
     // address = { 对象 } || 空字符串
     const address = wx.getStorageSync("address") || {};
     this.setData({
-      address
+      address, carts
     });
+    this.countAll(carts);
   },
   async handleFinalGet() {
     // 获取用户的授权状态
@@ -58,5 +56,35 @@ Page({
     const res = await chooseAddress();
     // 把数据存到缓存中
     wx.setStorageSync("address", res);
+  },
+  countAll(carts) {
+    /* 
+  1 获取缓存中的购物车数组
+  2 循环 
+    1 判断该商品的isChecked 是否为 true
+    2 获取每个商品的单价 * 要购买的数量
+    3 每个商品的总价 叠加计算 ++ 
+     */
+
+
+    let totalPrice = 0;
+    carts.forEach(v => {
+      if(v.isChecked){
+        totalPrice += v.nums * v.goods_price
+       
+this.setData({
+      totalPrice
+    })
+      }
+    })
+
+    // console.log(carts);
+    // let totalPrice = carts.reduce((beforSum, currenItem) => {
+    //   console.log(beforSum);
+    //   console.log(currenItem);
+    // }, totalPrice);
+  console.log(this.data.totalPrice);
+    
+    
   }
-});
+})
